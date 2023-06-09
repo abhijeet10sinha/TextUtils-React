9345af38109a0adca7a508357baa67f5fd0bfce1
Abhijeet Kumar Sinha <abhijeetsinha0410@gmail.com>
2023-06-10 01:10:05 +0530

    Update-1

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -42,9 +42,9 @@ export default function TextForm(props) {
     // setText("new text"); // Correct way to change the state
     return (
         <>
-        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
-            <h1 className='mb-4'>{props.heading}</h1>
-            <div className="mb-3"> 
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743', maxHeight: '300px'}}> 
+            <h1 className='mb-1'>{props.heading}</h1>
+            <div className="mb-2"> 
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
